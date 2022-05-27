6763ae1c40c10ebd6cf861bc95e9e59caa1b4782
Sirjamo1 <jam_how@hotmail.com>
2022-05-27 10:54:12 +1000

    added clickableimages for button with sound affects. added background image. disabled buttons that let player continue once game was complete. enabled same buttons once the reset button is clicked. simplified win/lose proccess since player can only click buttons now. made a few visual changes. added a couple @media queries

--- README.md
+++ README.md
@@ -1,7 +1,27 @@
-# Rock-Paper-Scissors
+# Rock-Paper-Scissors<br/>
 
-function computerPlay (line 2-11)<br/> Creates random number from 0-2 and returns Rock(0), Paper(1) and Scissors(2) <br/>Note: Made sure to put first letter upper case for user readability appearance.<br/><br/><br/>
-function playRound (line 14-36)<br/>Plays one game, and returns if you draw/win/lose and why in a string.<br/><br/><br/>
-function playerPrompt (line 39-48)<br/>Prompts player to input rock/paper/scissors then converts it to lowercase before changing the first character to upper case to match computer.<br/><br/><br/>
-function game (line 50-91)<br/>Uses a for loop to play until one player tallies 5 points, each round returns the current score and who won that round and why.<br/><br/><br/>
+HUGE UPDATE 27/05/2022<br/><br/>
+
+<ul>
+<li>Added clickable buttons with sound affects (from www.freesound.org) and images for buttons (from www.clipartmax.com) along with a background image (from www.wallpaperaccess.com)</li>
+  <ul>
+  <li>Rock sound by gregorquendel</li>
+  <li>PAper sound by xtyl33</li>
+  <li>Scissors sound by mxve</li>
+  <li>Win sound by mrrap4food</li>
+  <li>Lose sound by rocotilos</li>
+  </ul>
+<li>Disabled buttons that let player continue playing even when score was met / enable button when player clicks reset</li>
+<li>setAttribute to reflect color of text if you win/lose/draw</li>
+<li>made a few visual changes (box-shadow, transitions, transform scale when click etc.) </li>
+<li>Added a couple of @media queries for mobile and mobile/landscape</li>
+<li>Simplified if player beats computer now that there is only clickable choices</li>
+
+</ul>
+<br/><br/><br/>*****************************************************<br/><br/><br/>
+FOLLOWING INFO IS OUT OF DATE (left it there to know what i've changed)<br/><br/><br/>
+function computerPlay <br/> Creates random number from 0-2 and returns Rock(0), Paper(1) and Scissors(2) <br/>Note: Made sure to put first letter upper case for user readability appearance.<br/><br/><br/>
+function playRound <br/>Plays one game, and returns if you draw/win/lose and why in a string.<br/><br/><br/>
+function playerPrompt <br/>Prompts player to input rock/paper/scissors then converts it to lowercase before changing the first character to upper case to match computer.<br/><br/><br/>
+function game <br/>Uses a for loop to play until one player tallies 5 points, each round returns the current score and who won that round and why.<br/><br/><br/>
 NOTE: currently have to play through devTools (Ctrl + Shift + i) by typing in console game(); (will add html and css in future for playability)<br/><br/><br/>
--- js/script.js
+++ js/script.js
@@ -10,84 +10,187 @@ function computerPlay() {
   }
 }
 
-// plays one game, and returns if you draw/win/lose and why in a string
+let playerScore = 0; //player score start at 0
+let computerScore = 0; //computer score starts at 0
+let draw = 0;
+// win/lose/draw results with string and + 1 to score
 function playRound(playerSelection, computerSelection) {
-  if (
-    (playerSelection === "Rock" && computerSelection === "Rock") ||
-    (playerSelection === "Paper" && computerSelection === "Paper") ||
-    (playerSelection === "Scissors" && computerSelection === "Scissors")
-  ) {
-    return `Draw! ${playerSelection} is equal to ${computerSelection}.`;
+  const winLoseRound = document.querySelector("#winLoseRound");
+  if (playerSelection === computerSelection) {
+    return (
+      winLoseRound.setAttribute("style", "color: black"),
+      (winLoseRound.textContent = `Draw! ${playerSelection} is equal to ${computerSelection}.`),
+      (draw += 1)
+    );
   } else if (
     (playerSelection === "Rock" && computerSelection === "Scissors") ||
     (playerSelection === "Paper" && computerSelection === "Rock") ||
     (playerSelection === "Scissors" && computerSelection === "Paper")
   ) {
-    return `You Win! ${playerSelection} beats ${computerSelection}.`;
+    return (
+      winLoseRound.setAttribute("style", "color: green;"),
+      (winLoseRound.textContent = `You Win! ${playerSelection} beats ${computerSelection}.`),
+      (playerScore += 1)
+    );
   } else if (
     (playerSelection === "Rock" && computerSelection === "Paper") ||
     (playerSelection === "Paper" && computerSelection === "Scissors") ||
     (playerSelection === "Scissors" && computerSelection === "Rock")
   ) {
-    return `You Lose! ${computerSelection} beats ${playerSelection}.`;
-  } else {
-    return `${playerSelection} is invalid. Please enter either Rock, Paper or Scissors.`;
+    return (
+      winLoseRound.setAttribute("style", "color: red;"),
+      (winLoseRound.textContent = `You Lose! ${computerSelection} beats ${playerSelection}.`),
+      (computerScore += 1)
+    );
   }
 }
+// list of audio
+const rockSound = new Audio(
+  "sounds/424997__gregorquendel__rocks-effects-source-recordings-05.wav"
+);
+const paperSound = new Audio("/sounds/68221__xtyl33__paper2.wav");
+const scissorsSound = new Audio("/sounds/372246__mxve__scissors.wav");
+const winSound = new Audio(
+  "/sounds/619035__mrrap4food__burst-of-a-short-clap.mp3"
+);
+const loseSound = new Audio("/sounds/178875__rocotilos__you-lose-evil.wav");
+// click of button triggers player selection, computer selection and start of round
+const buttons = document.querySelectorAll("#buttonDiv");
+buttons.forEach((button) => {
+  button.addEventListener("click", (el) => {
+    //plays sound affect
+    if (el.target.id == "rock") {
+      rockSound.play();
+    } else if (el.target.id == "paper") {
+      paperSound.play();
+    } else if (el.target.id == "scissors") {
+      scissorsSound.play();
+    }
+    // changes first letter to capital to match against computer selection
+    const playerSelection =
+      el.target.id.charAt(0).toUpperCase() + el.target.id.slice(1);
+    const computerSelection = computerPlay();
+    //starts playRound function
+    playRound(playerSelection, computerSelection);
+    //prints current scoreboard
+    const scoreDiv = document.querySelector("#scoreDiv");
+    scoreDiv.textContent = `Player Score : ${playerScore} Computer Score : ${computerScore} Draw : ${draw}`;
+    //prints final scoreboard when first player wins five games
+    const winLoseTotal = document.querySelector("#winLoseTotal");
+    if (playerScore >= 5) {
+      // alerts player they have won with final score and string and disables rock, paper, scissor buttons
+      return (
+        (winLoseTotal.textContent = `You Won!  Player: ${playerScore}  Computer: ${computerScore}`),
+        winLoseTotal.setAttribute("style", "color: green;"),
+        disableBtn(),
+        winSound.play()
+      );
+    } else if (computerScore >= 5) {
+      // alerts player they have lost with final score, audio, string and disables rock paper scissors buttons
+      return (
+        (winLoseTotal.textContent = `You Lose!  Player: ${playerScore}  Computer: ${computerScore}`),
+        winLoseTotal.setAttribute("style", "color: red;"),
+        disableBtn(),
+        loseSound.play()
+      );
+    }
+  });
+});
+// resets scores back to zero and enables rock, paper, scissors buttons
+const resetDiv = document.querySelector("#resetDiv");
+resetDiv.addEventListener("click", (el) => {
+  return (
+    (scoreDiv.textContent = `Player Score : 0 Computer Score : 0 Draw : 0`),
+    (playerScore = 0),
+    (computerScore = 0),
+    (draw = 0),
+    (winLoseRound.textContent = `again?`),
+    (winLoseTotal.textContent = `Let's Go!`),
+    enableBtn()
+  );
+});
+// disables rock, paper, scissor buttons
+function disableBtn() {
+  document
+    .querySelectorAll(".buttons")
+    .forEach((button) => button.setAttribute("disabled", true));
+}
+// enables rock, paper, scissor buttons
+function enableBtn() {
+  document
+    .querySelectorAll(".buttons")
+    .forEach((button) => button.removeAttribute("disabled"));
+}
+
+//
+//
+//
+//                    *******OLD CODE DON"T NEED!!!***********
 
 // prompts player to input rock/paper/scissors then converts it to lowercase before changing the first character to upper case to match computer
-function playerPrompt() {
-  let playerInput = prompt("Please Choose Rock, Paper or Scissors");
-  playerInput = playerInput.toLowerCase();
-  let playerInputCased = " ";
-  if ((playerInput === "rock", "paper", "scissors")) {
-    playerInputCased =
-      playerInput.charAt(0).toUpperCase() + playerInput.slice(1);
-  }
-  return playerInputCased;
-}
 
-function game() {
-  let playerScore = 0; //player score start at 0
-  let computerScore = 0; //computer score starts at 0
-  for (let i = 0; i < 5; i++) {
-    // play for 5 games
-    const playerSelection = playerPrompt();
-    const computerSelection = computerPlay();
-    console.log(playRound(playerSelection, computerSelection)); //  1 playRound function and console.log string
-    if (playRound(playerSelection, computerSelection).includes(`Win!`)) {
-      // if the string `Win!` is found in the return string plus 1 to player score
-      playerScore += 1;
-    } else if (
-      playRound(playerSelection, computerSelection).includes(`Lose!`)
-    ) {
-      // if the string `Lose!` is found in the return string plus 1 to computer
-      computerScore += 1;
-    } else if (
-      playRound(playerSelection, computerSelection).includes(`Draw!`)
-    ) {
-      // if the string `Draw!` is found in the return string plus 0 and i-- (to play another round)
-      i--;
-    } else if (
-      playRound(playerSelection, computerSelection).includes(`invalid`)
-    ) {
-      // if the string `invalid` is found in the return string plus 0 and i-- (to play another round)
-      i--;
-    }
-    console.log(`Player: ${playerScore}  Computer: ${computerScore}`);
-    // shows player/computer score after each round
-  }
-  if (playerScore > computerScore) {
-    // alerts player they have won with final score and string
-    return alert(
-      `You Won!  Player: ${playerScore}  Computer: ${computerScore}`
-    );
-  } else if (playerScore < computerScore) {
-    // alerts player they have lost with final score and string
-    return alert(
-      `You Lose!  Player: ${playerScore}  Computer: ${computerScore}`
-    );
-  }
-}
+// don't need the below with clickable buttons
 
-// need to add html and css for user playability
+// function playerPrompt() {
+//   let playerInput = prompt("Please Choose Rock, Paper or Scissors");
+//   playerInput = playerInput.toLowerCase();
+//   let playerInputCased = " ";
+//   if ((playerInput === "rock", "paper", "scissors")) {
+//     playerInputCased =
+//       playerInput.charAt(0).toUpperCase() + playerInput.slice(1);
+//   }
+//   return playerInputCased;
+// }
+
+/////// DON"T NEED THIS EITHER
+
+// console.log(playRound(playerSelection, computerSelection)); //  1 playRound function and console.log string
+//       if (playRound(playerSelection, computerSelection).includes(`Win!`)) {
+//         // if the string `Win!` is found in the return string plus 1 to player score
+//         playerScore += 1;
+//       } else if (
+//         playRound(playerSelection, computerSelection).includes(`Lose!`)
+//       ) {
+//         // if the string `Lose!` is found in the return string plus 1 to computer
+//         computerScore += 1;
+//       } else if (
+//         playRound(playerSelection, computerSelection).includes(`Draw!`)
+//       ) {
+//         // if the string `Draw!` is found in the return string plus 0 and i-- (to play another round)
+//         i--;
+//       } else if (
+//         playRound(playerSelection, computerSelection).includes(`invalid`)
+//       ) {
+//         // if the string `invalid` is found in the return string plus 0 and i-- (to play another round)
+//         i--;
+//       }
+//       console.log(`Player: ${playerScore}  Computer: ${computerScore}`);
+//       // shows player/computer score after each round
+//     }
+
+////  ALSO DON'T NEED THIS
+
+// plays one game, and returns if you draw/win/lose and why in a string
+// function playRound(playerSelection, computerSelection) {
+//   if (
+//     (playerSelection === "Rock" && computerSelection === "Rock") ||
+//     (playerSelection === "Paper" && computerSelection === "Paper") ||
+//     (playerSelection === "Scissors" && computerSelection === "Scissors")
+//   ) {
+//     return `Draw! ${playerSelection} is equal to ${computerSelection}.`;
+//   } else if (
+//     (playerSelection === "Rock" && computerSelection === "Scissors") ||
+//     (playerSelection === "Paper" && computerSelection === "Rock") ||
+//     (playerSelection === "Scissors" && computerSelection === "Paper")
+//   ) {
+//     return `You Win! ${playerSelection} beats ${computerSelection}.`;
+//   } else if (
+//     (playerSelection === "Rock" && computerSelection === "Paper") ||
+//     (playerSelection === "Paper" && computerSelection === "Scissors") ||
+//     (playerSelection === "Scissors" && computerSelection === "Rock")
+//   ) {
+//     return `You Lose! ${computerSelection} beats ${playerSelection}.`;
+//   } else {
+//     return `${playerSelection} is invalid. Please enter either Rock, Paper or Scissors.`;
+//   }
+// }
